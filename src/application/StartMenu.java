6482a516b076cdfe1b16cafe83d11caf13b73ea3
OwenJose <OwenJose@172.20.200.150>
2015-10-27 11:07:17 +0100

    making it pretty

--- src/application/StartMenu.java
+++ src/application/StartMenu.java
@@ -1,69 +1,59 @@
 package application;
 
-import javafx.application.Application;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
-public class StartMenu extends Application{
+public class StartMenu extends BorderPane{
 
-
-	@Override
-	public void start(Stage primaryStage) {
-		
-		BorderPane rootNode = new BorderPane();
-		Scene scene = new Scene(rootNode);
-		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		primaryStage.setTitle("Start Menu");
-		
-		Label headLine = new Label("Memory");
+	Label headLine, playersLabel, fieldLabel;
+	Button  playButton, resetButton, addButton, createButton;
+	ComboBox<String> choosePlayers, playFields;
+	VBox alignmentBox;
+	HBox bottomButtons, fieldOption, playersBox, labelBox; 
+	
+	StartMenu()
+		{
+	
+		headLine = new Label("Memory");
 		headLine.setAlignment(Pos.CENTER);
 		
-		Label playersLabel = new Label("Add number of players");
-		Label fieldLabel = new Label("Play field selection");
+		playersLabel = new Label("Add number of players");
+		fieldLabel = new Label("Play field selection");
 		
-		Button playButton = new Button("Let's play");
-		Button resetButton = new Button("Reset");
-		Button addButton = new Button("Add");
-		Button createButton = new Button("Create");
+		playButton = new Button("Let's play");
+		resetButton = new Button("Reset");
+		addButton = new Button("Add");
+		createButton = new Button("Create");
 		
-		ComboBox<String> choosePlayers = new ComboBox<>();
-		ComboBox<String> playFields = new ComboBox<>();
+		choosePlayers = new ComboBox<>();
+		playFields = new ComboBox<>();
 		
-		HBox labelBox = new HBox();
+		labelBox = new HBox();
 		labelBox.setAlignment(Pos.CENTER);
 		labelBox.getChildren().add(headLine);
-		rootNode.setTop(labelBox);
-		HBox playersBox = new HBox(5);
+		setTop(labelBox);
+		playersBox = new HBox(5);
 		playersBox.setAlignment(Pos.CENTER);
 		playersBox.getChildren().addAll(playersLabel, choosePlayers, addButton, createButton);
-		HBox fieldOption = new HBox(5);
+		fieldOption = new HBox(5);
 		fieldOption.setAlignment(Pos.CENTER);
 		fieldOption.getChildren().addAll(fieldLabel, playFields);
-		HBox bottomButtons = new HBox(5);
+		bottomButtons = new HBox(5);
 		bottomButtons.setAlignment(Pos.BASELINE_RIGHT);
 		bottomButtons.getChildren().addAll(resetButton, playButton);
 		
-		VBox alignmentBox = new VBox(5);
+		alignmentBox = new VBox(5);
 		alignmentBox.setAlignment(Pos.CENTER);
 		alignmentBox.getChildren().addAll(playersBox, fieldOption);
 		
-		rootNode.setCenter(alignmentBox);
-		rootNode.setBottom(bottomButtons);
-
+		setCenter(alignmentBox);
+		setBottom(bottomButtons);
+		}
 		
 		}
 	
-	public static void main(String[] args) {
-		launch(args);
-	}
-
-}
