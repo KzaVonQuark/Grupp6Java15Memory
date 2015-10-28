1e91912ba2bb9ea1336742d5fe85e507d9416f6f
Ian Pratt <ian.pratt@yh.nackademin.se>
2015-10-28 12:53:51 +0100

    padding

--- src/application/GameBoard.java
+++ src/application/GameBoard.java
@@ -1,5 +1,6 @@
 package application;
 
+import javafx.geometry.Insets;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -13,6 +14,7 @@ public class GameBoard extends BorderPane {
 		grid = new GridPane();
 		setCenter(grid);
 		decks = new Deck(10, "frontimage2");
+		grid.setPadding(new Insets(25, 25, 25, 25));
 
 		for (int i = 0; i < decks.getDeckSize(); i++) {
 			ImageView imageView = new ImageView(decks.dealCard().getFrontImage());
