672776f8831bbc1e2d51921fe312834c029134e9
OwenJose <OwenJose@Kockens>
2015-10-28 16:54:42 +0100

    change of names and design

--- src/application/StartMenu.java
+++ src/application/StartMenu.java
@@ -13,14 +13,14 @@ import javafx.scene.text.Font;
 public class StartMenu extends BorderPane{
 
 	Label headLine, playersLabel, fieldLabel;
-	Button  playButton, resetButton, addButton, createButton;
+	Button  playButton, ExitButton, newGameButton, createButton;
 	ComboBox<String> choosePlayers, playFields;
-	VBox alignmentBox, bottomButtons;
-	HBox fieldOption, playersBox, labelBox; 
+	VBox alignmentBox, bottomButtons, fieldOption;
+	HBox playersBox, labelBox; 
 	
 	StartMenu()
 		{
-	
+		
 		headLine = new Label("Memory");
 		headLine.setAlignment(Pos.CENTER);
 		headLine.setTextFill(Color.WHITESMOKE);
@@ -39,23 +39,23 @@ public class StartMenu extends BorderPane{
 		playButton.setTextFill(Color.WHITESMOKE);
         playButton.setStyle("-fx-background-color: rgba(0, 0, 0, 0);");
         playButton.setFont(Font.font(20));
-		resetButton = new Button("Reset");
-		resetButton.setTextFill(Color.WHITESMOKE);
-		resetButton.setStyle("-fx-background-color: rgba(0, 0, 0, 0);");
-		resetButton.setFont(Font.font(20));
-		addButton = new Button("Add");
-		addButton.setTextFill(Color.WHITESMOKE);
-		addButton.setStyle("-fx-background-color: rgba(0, 0, 0, 0);");
-		addButton.setFont(Font.font(20));
+		ExitButton = new Button("Leave Game");
+		ExitButton.setTextFill(playButton.getTextFill());
+		ExitButton.setStyle(playButton.getStyle());
+		ExitButton.setFont(Font.font(20));
+		newGameButton = new Button("New Game");
+		newGameButton.setTextFill(playButton.getTextFill());
+		newGameButton.setStyle(playButton.getStyle());
+		newGameButton.setFont(Font.font(20));
 		createButton = new Button("Create");
-		createButton.setTextFill(Color.WHITESMOKE);
-		createButton.setStyle("-fx-background-color: rgba(0, 0, 0, 0);");
+		createButton.setTextFill(playButton.getTextFill());
+		createButton.setStyle(playButton.getStyle());
 		createButton.setFont(Font.font(20));
 		
 		choosePlayers = new ComboBox<>();
-		choosePlayers.setPrefHeight(40);
+		choosePlayers.setPromptText("players");
 		playFields = new ComboBox<>();
-		playFields.setPrefHeight(40);
+		playFields.setPromptText("theme");
 		
 		labelBox = new HBox();
 		labelBox.setAlignment(Pos.CENTER);
@@ -65,21 +65,21 @@ public class StartMenu extends BorderPane{
 		playersBox.setAlignment(Pos.TOP_CENTER);
 		playersBox.getChildren().add(playersLabel);
 		
-		fieldOption = new HBox(5);
+		fieldOption = new VBox(5);
 		fieldOption.setAlignment(Pos.TOP_CENTER);
-		fieldOption.getChildren().add(fieldLabel);
+		fieldOption.getChildren().addAll(fieldLabel, playersLabel);
 		
 		bottomButtons = new VBox(5);
 		bottomButtons.setAlignment(Pos.TOP_LEFT);
-		bottomButtons.getChildren().addAll(choosePlayers, playFields);
 		
 		alignmentBox = new VBox(5);
 		alignmentBox.setAlignment(Pos.TOP_CENTER);
-		alignmentBox.getChildren().addAll(addButton, createButton, playersBox, fieldOption, resetButton, playButton);
+		alignmentBox.getChildren().addAll(newGameButton, createButton, playButton, ExitButton);
 		
 		setTop(labelBox);
 		setLeft(alignmentBox);
 		setCenter(bottomButtons);
+		setRight(fieldOption);
 		
 		setStyle("-fx-background-image: url(\"images/background.jpg\"); -fx-backgroundsize: 800,600;");
 		}
