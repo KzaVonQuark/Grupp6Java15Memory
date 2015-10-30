10e5b6b11f8e2649b0e3eaa3817b21c6e1076a00
OwenJose <OwenJose@Kockens.lan>
2015-10-30 12:40:20 +0100

    Some more designe changes, Added the created players in the choose player combobox and the can be added to the list of participants

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,16 +1,23 @@
 package application;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application {
+	static int timePassed = 0;
+	static int timeToSee = 0;
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -18,7 +25,6 @@ public class Main extends Application {
 
 			GameBoard gameBoard = new GameBoard();
 			FileManager fm = new FileManager();
-			Rules rules = new Rules();
 
 			FreePane root = new FreePane();
 			Scene scene = new Scene(root, 800, 600);
@@ -29,9 +35,8 @@ public class Main extends Application {
 			primaryStage.show();
 			primaryStage.setTitle("Java15:Grupp6:Memory");
 
-			ObservableList<String> playerEntries = FXCollections.observableArrayList(fm.loadNames());
-			ComboBox<String> getPlayer = new ComboBox<String>(playerEntries);
-			ObservableList<String> scoreEntries = FXCollections.observableArrayList("Highest point", "Least Moves");
+			ObservableList<String> scoreEntries = FXCollections.observableArrayList("Highest point", "Least Moves",
+					"Fastest Time");
 			ComboBox<String> scoreType = new ComboBox<String>(scoreEntries);
 			ObservableList<Player> highScoreEntries = FXCollections.observableArrayList();
 			ListView<Player> HighScoreList = new ListView<Player>();
@@ -51,14 +56,18 @@ public class Main extends Application {
 			 */
 			start.newGameButton.setOnAction(event -> {
 				start.centerBox.getChildren().clear();
-				start.centerBox.getChildren().addAll(start.choosePlayers, start.playFields);
-				scoreType.setValue("Choose highscore");
-				start.centerBox.getChildren().addAll(scoreType, HighScoreList);
+				start.centerBox.getChildren().addAll(start.choosePlayers, start.playFields,scoreType, HighScoreList);
+				scoreType.setPromptText("Highscores");
+				start.choosePlayers.setOnAction(event1 -> {
+					start.fieldOption.getChildren().clear();
+					start.fieldOption.getChildren().addAll(start.playersHeadLine, start.playersLabel);
+					start.playersLabel.setText(start.playersLabel.getText() + start.choosePlayers.getValue() + "\n");				
+				});
 				scoreType.setOnAction(event2 -> {
-					highScoreEntries.clear();
-					HighScoreList.setItems(highScoreEntries);
 					highScoreEntries.setAll(fm.loadHighScore(scoreType.getValue()));
 					HighScoreList.setItems(highScoreEntries);
+					start.fieldOption.getChildren().clear();
+					start.fieldOption.getChildren().add(HighScoreList);
 				});
 			});
 
@@ -66,42 +75,47 @@ public class Main extends Application {
 				start.centerBox.getChildren().clear();
 				start.centerBox.getChildren().add(start.creatorTexfield);
 			});
-
+			
 			start.creatorTexfield.setOnAction(event -> {
-
 				start.playersLabel.setText(start.playersLabel.getText() + start.creatorTexfield.getText() + "\n");
 				start.creatorTexfield.clear();
 				start.centerBox.getChildren().clear();
 			});
-
-			start.ExitButton.setOnAction(event -> {
+			
+			start.exitButton.setOnAction(event -> {
 				Platform.exit();
 			});
 
-			gameBoard.grid.setOnMouseClicked(me -> {
+			gameBoard.grid.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				CardImageView cardOne = null;
+				CardImageView cardTwo = null;
 
+				@Override
+				public void handle(MouseEvent me) {
+					// TODO Auto-generated method stub
 					try {
 						CardImageView cardIv = (CardImageView) me.getPickResult().getIntersectedNode();
-					if (!cardIv.equals(rules.getCardOne())) {// Check if player
+						if (!cardIv.equals(cardOne)) {// Check if player
 							// choose
 							// same card
-						if (rules.getCardOne() == null) {
-							rules.setCardOne(cardIv);
-						} else if (rules.getCardTwo() == null) {
-							rules.setCardTwo(cardIv);
+							if (cardOne == null) {
+								cardOne = cardIv;
+							} else if (cardTwo == null) {
+								cardTwo = cardIv;
 							}
-						if (!rules.getCardOne().isFlipped()) {
-							rules.setCardOne(cardIv);
-							System.out.println("Card 1 Selected! (" + rules.getCardOne().getCard().getValue() + ")");
-							rules.getCardOne().Flip();
-						} else if (!rules.getCardTwo().isFlipped()) {
-							rules.setCardTwo(cardIv);
-							rules.getCardTwo().Flip();
-							System.out.println("Card 2 Selected! (" + rules.getCardTwo().getCard().getValue() + ")");
-							rules.confirmPair(rules.getCardOne(), rules.getCardTwo());
+							if (!cardOne.isFlipped()) {
+								cardOne = cardIv;
+								System.out.println("Card 1 Selected! (" + cardOne.getCard().getValue() + ")");
+								cardOne.Flip();
+							} else if (!cardTwo.isFlipped()) {
+								cardTwo = cardIv;
+								cardTwo.Flip();
+								System.out.println("Card 2 Selected! (" + cardTwo.getCard().getValue() + ")");
+								ConfirmPair(cardOne, cardTwo);
 							}
 						}
 					} catch (ClassCastException e) {
+					}
 				}
 			});
 
@@ -110,7 +124,32 @@ public class Main extends Application {
 		}
 	}
 
+	public static void ConfirmPair(CardImageView a, CardImageView b) {
+		if (a.getCard().getValue() == b.getCard().getValue())
+			System.out.println("Du hittade ett par!");
+		else
+			System.out.println("Du hittade inget par!");
+		Timeline delay = new Timeline();
+		delay.setCycleCount(Timeline.INDEFINITE);
 
+		KeyFrame cardFlip = new KeyFrame(Duration.seconds(1), e -> {
+			if (timePassed > timeToSee) {
+				if (a.getCard().getValue() == b.getCard().getValue()) {
+					a.Remove();
+					b.Remove();
+				} else {
+					a.Flip();
+					b.Flip();
+				}
+				timePassed = 0;
+				delay.stop();
+			} else {
+				timePassed++;
+			}
+		});
+		delay.getKeyFrames().add(cardFlip);
+		delay.play();
+	}
 
 	public static void main(String[] args) {
 		launch(args);
