ff6027d77815937cbcd32ac005ee2967a699bee8
MasihBandari <Masih_bandari@hotmail.com>
2015-11-02 10:59:06 +0100

    Optimized Ian's Gameboard code.

--- src/application/GameBoard.java
+++ src/application/GameBoard.java
@@ -1,42 +1,42 @@
 package application;
 
 import java.util.Queue;
-import javafx.geometry.Insets;
+
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
 public class GameBoard extends BorderPane {
 	GridPane grid;
 	Deck decks;
-	
+
 	// % = Added by Linus.
 	Queue<Player> q; // %
 
-	
 	GameBoard() { // Get players from "participants". // % Player[] players
 		/*
-		for (Player player : players) {
-			q.add(player);
-		} 									//%
-*/
+		 * for (Player player : players) { q.add(player); } //%
+		 */
 		grid = new GridPane();
 		setCenter(grid);
-		decks = new Deck(12, "frontimage2");
+		decks = new Deck(36, "frontimage2");
 		grid.setHgap(10);
 		grid.setVgap(10);
-		
-		
-		
 
+		int row = 0;
+		int col = 0;
 		for (int i = 0; i < decks.getDeckSize(); i++) {
-			CardImageView imageView = new CardImageView(decks.dealCard(i).getBackImage(), decks.dealCard(i).getValue());
+			if (col > 5) {
+				col = 0;
+				row++;
+			}
+			CardImageView imageView = new CardImageView(decks.dealCard(i).getFrontImage(), decks.dealCard(i));
 			imageView.setFitHeight(100);
 			imageView.setFitWidth(100);
-			grid.add(imageView, i, 0);
-			
-			if(i >= 4){
-				return;
-			}
+			grid.add(imageView, col, row);
+			col++;
+			// if(i >= 4){
+			// return;
+			// }
 		}
 	}
 
